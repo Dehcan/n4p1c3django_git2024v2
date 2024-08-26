ee0d1827fab4c7972a5f1a4c3c774b9beb0be9ea
Lost020 <luis.javier.gonzalez.arias@gmail.com>
2024-08-26 17:47:53 -0400

    Rama app3 terminada

---
+++ app3/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Soy el index de la app3, Lost020</h1>")
